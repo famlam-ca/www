15e761d8fdd9b68c52c4f578f370554dfca63551
SlickYeet <fglp.cm@gmail.com>
2023-10-02 14:05:06 -0700

    account page sidebar fix

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -5,14 +5,13 @@ import Sidebar from "@/components/Navigation/Sidebar";
 
 const Account = () => {
   return (
-    <main className="mt-0 h-screen bg-backgroundLight  dark:bg-backgroundDark">
-      <section className="mx-4 grid gap-7 max-md:grid-cols-dashboardMd md:mx-8 lg:m-auto lg:ml-0 lg:mr-8 lg:grid-cols-dashboardLg xl:grid-cols-dashboardXl ">
-        <Sidebar />
+    <Sidebar>
+      <section className="max-md:grid-cols-dashboardMd xl:grid-cols-dashboardXl mx-4 grid gap-7 md:mx-8 lg:m-auto lg:ml-0 lg:mr-8 lg:grid-cols-dashboardLg ">
         <h1>Account</h1>
 
         <Header />
       </section>
-    </main>
+    </Sidebar>
   );
 };
 
