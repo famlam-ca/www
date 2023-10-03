05041f74bc4daeea25d50ffd3500488e00fb1491
SlickYeet <fglp.cm@gmail.com>
2023-10-03 08:31:15 -0700

    logo clean up

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,11 +5,7 @@ import { useRouter } from "next/navigation";
 
 import logo from "@/public/images/logo512.png";
 
-interface LogoProps {
-  className?: string;
-}
-
-const Logo: React.FC<LogoProps> = ({ className }) => {
+const Logo = () => {
   const router = useRouter();
 
   return (
@@ -17,7 +13,7 @@ const Logo: React.FC<LogoProps> = ({ className }) => {
       onClick={() => router.push("/")}
       className="title-font mr-8 flex items-center justify-center gap-4 font-medium md:mb-0"
     >
-      <Image property="1" src={logo} alt="Logo" className="h-20 w-20" />
+      <Image src={logo} alt="Logo" className="h-20 w-20" />
       <h2 className="text-2xl font-semibold text-dark dark:text-light">
         H<span className="text-primary">HN</span>
       </h2>
